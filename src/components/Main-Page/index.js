a1851de7265f3f7184ba49d801c0bde4a284e453
ramadika <rzdka18@gmail.com>
2021-07-16 19:53:30 +0700

    Report of Amount

--- src/components/Main-Page/index.js
+++ src/components/Main-Page/index.js
@@ -8,8 +8,15 @@ export default function App() {
     <div className="App">
       <h2>Programming Challenge</h2>
       <button onClick={random}>Generate</button>
-      <button onClick={txtFile} id="download">Download Result</button>
       <span id="result"></span>
+      <button onClick={file} id="download">Download Result</button>
+      <button onClick={display} id="display">Report</button>
+      <div id="report">
+        <h4>Alphabetical String <span id="string"></span></h4>
+        <h4>Real Numbers <span id="real"></span></h4>
+        <h4>Integers <span id="integers"></span></h4>
+        <h4>Alphanumerics <span id="alphanumerics"></span></h4>
+      </div>
     </div>
   )
 }
@@ -18,10 +25,21 @@ function randomAlphanumerics() {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    var length = Math.floor((Math.random() * 10) + 1);
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    var length = Math.floor((Math.random() * charactersLength) + 1);
+    for ( var j = 0; j < Math.floor((Math.random() * charactersLength) + 1); j++ ){
+      for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      result += ",";
+      if (localStorage.getItem("Alphanumerics") === null) {
+        localStorage.setItem('Alphanumerics', j);
+      }else {
+        var report = localStorage.getItem('Alphanumerics');
+        report = parseInt(report) + j; 
+        localStorage.setItem('Alphanumerics', report);
+      }
     }
+
     return result;
 }
 
@@ -29,9 +47,19 @@ function randomString() {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     var charactersLength = characters.length;
-    var length = Math.floor((Math.random() * 10) + 1);
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    var length = Math.floor((Math.random() * charactersLength) + 1);
+    for ( var j = 0; j < Math.floor((Math.random() * charactersLength) + 1); j++ ){
+      for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      result += ",";
+      if (localStorage.getItem("String") === null) {
+        localStorage.setItem('String', j);
+      }else {
+        var report = localStorage.getItem('String');
+        report = parseInt(report) + j; 
+        localStorage.setItem('String', report);
+      }
     }
     return result;
 }
@@ -40,9 +68,19 @@ function randomInteger() {
     var result           = '';
     var characters       = '0123456789';
     var charactersLength = characters.length;
-    var length = Math.floor((Math.random() * 10) + 1);
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    var length = Math.floor((Math.random() * charactersLength) + 1);
+    for ( var j = 0; j < Math.floor((Math.random() * charactersLength) + 1); j++ ){
+      for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      result += ",";
+      if (localStorage.getItem("Integers") === null) {
+        localStorage.setItem('Integers', j);
+      }else {
+        var report = localStorage.getItem('Integers');
+        report = parseInt(report) + j; 
+        localStorage.setItem('Integers', report);
+      }
     }
     return result;
 }
@@ -52,6 +90,13 @@ function randomReal() {
     var length = Math.floor((Math.random() * 10) + 1);
     for ( var i = 0; i <= length; i++ ) {
       result.push(Math.random() * length);
+      if (localStorage.getItem("Real") === null) {
+        localStorage.setItem('Real', i);
+      }else {
+        var report = localStorage.getItem('Real');
+        report = parseInt(report) + i; 
+        localStorage.setItem('Real', report);
+      }
   }
   return result;
 }
@@ -85,13 +130,27 @@ function random() {
     shuffle(result)
     document.getElementById("result").innerHTML = result;
     document.getElementById("download").style.visibility = 'visible';
+    document.getElementById("display").style.visibility = 'visible';
+
+    // localStorage.removeItem('String');
+    // localStorage.removeItem('Real');
+    // localStorage.removeItem('Integers');
+    // localStorage.removeItem('Alphanumerics');
 }
 
-function txtFile() {
+function file() {
   const element = document.createElement("a");
   const file = new Blob([document.getElementById('result').innerHTML], {type: 'text/plain'});
   element.href = URL.createObjectURL(file);
   element.download = "myFile.txt";
   document.body.appendChild(element); // Required for this to work in FireFox
   element.click();
+}
+
+function display() {
+  document.getElementById("report").style.visibility = 'visible';
+  document.getElementById("string").innerHTML = localStorage.getItem('String');
+  document.getElementById("real").innerHTML = localStorage.getItem('Real');
+  document.getElementById("integers").innerHTML = localStorage.getItem('Integers');
+  document.getElementById("alphanumerics").innerHTML = localStorage.getItem('Alphanumerics');
 }
